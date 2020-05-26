b13247bf515ccaef45b26b3ab7a12e9b0329ab10
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2020-05-27 02:47:51 +0800

    Fix bug in Bettor

--- autobet/bettor.py
+++ autobet/bettor.py
@@ -1,4 +1,5 @@
 class Bettor:
+	MIN_BET_AMOUNT = 100
 	SMALL_BET_AMOUNT = 1000
 	MEDIUM_BET_AMOUNT = 5000
 	BIG_BET_AMOUNT = 10000
@@ -8,9 +9,9 @@ class Bettor:
 		return 1 / sum(win_percentages)
 
 	def bet(odds):
+		lowest_odd_idx = odds.index(min(odds))
 		if Bettor.expected_return(odds) < 1:
-			return (0, 1)
-			lowest_odd_idx = odds.index(min(odds))
+			return (lowest_odd_idx, Bettor.MIN_BET_AMOUNT)
 		elif Bettor.expected_return(odds) < 1.1:
 			return (lowest_odd_idx, Bettor.SMALL_BET_AMOUNT)
 		elif Bettor.expected_return(odds) < 1.2:
