fc1dc61831c069eb2d999a507063f3781c3339e4
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2020-06-01 02:50:23 +0800

    Add check for place bet screen

--- autobet/app.py
+++ autobet/app.py
@@ -55,22 +55,33 @@ class App:
 			log("Not on betting screen. Waiting for 10s.")
 			time.sleep(10)
 			return
+
 		Clicker.click_place_bet_start_screen()
+
+		if not at_place_bet_screen():
+			time.sleep(3)
+		if not at_place_bet_screen():
+			log('Game bug. Exiting and re-entering...')
+			Clicker.exit_and_reenter()
+			return
+
 		odds = Reader.read_odds()
 		self.odds.append(odds)
 		bet_position, bet_amount = Bettor.bet(odds)
 		Clicker.place_bet(bet_position, bet_amount)
 		log(f'Placing bet on {bet_position} for {bet_amount}')
 		time.sleep(HORSE_RACE_DURATION_SECONDS)
+
 		# Sometimes it lags here, need to exit and re-enter betting game
 		if not at_results_screen():
 			time.sleep(3)
 		if not at_results_screen():
 			log('Game bug. Exiting and re-entering...')
 			Clicker.exit_and_reenter()
-		else:
-			winning = Reader.read_winning()
-			net_won = winning - bet_amount
-			log(f'Made ${net_won}')
-			self.winnings.append(net_won)
-			Clicker.click_bet_again()
+			return
+
+		winning = Reader.read_winning()
+		net_won = winning - bet_amount
+		log(f'Made ${net_won}')
+		self.winnings.append(net_won)
+		Clicker.click_bet_again()
