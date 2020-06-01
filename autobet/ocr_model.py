f51d919e097081df021eb5f71e3bb2b363e6fa31
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2020-06-01 05:38:35 +0000

    Change training code

--- autobet/ocr_model.py
+++ autobet/ocr_model.py
@@ -8,20 +8,30 @@ MAX_ODDS = 30
 INPUT_SHAPE = (int(PLACE_BET_SCREEN_ODDS_WIDTH * SCREEN_WIDTH), int(PLACE_BET_SCREEN_ODDS_HEIGHT * SCREEN_HEIGHT), 3)
 
 def model():
-	# ~1 million parameters
-	model = Sequential()
-	model.add(Conv2D(32, kernel_size=(3, 3),
-	                 activation='relu',
-	                 input_shape=INPUT_SHAPE))
-	model.add(Conv2D(64, kernel_size=(2, 2),
-	                 activation='relu'))
-	model.add(Flatten())
-	model.add(Dense(16, activation='relu'))
-	model.add(Dense(32, activation='relu'))
-	model.add(Dense(64, activation='relu'))
-	model.add(Dense(MAX_ODDS, activation='softmax'))
+        # ~1 million parameters
+        model = Sequential()
+        model.add(Conv2D(32, kernel_size=(3, 3),
+                         activation='relu',
+                         input_shape=INPUT_SHAPE))
+        model.add(Conv2D(32, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
 
-	model.compile(optimizer='adam',
-              loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-	return model
+        model.add(Conv2D(64, (3, 3), padding='same'))
+        model.add(Activation('relu'))
+        model.add(Conv2D(64, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Flatten())
+        model.add(Dense(512))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(MAX_ODDS, activation='softmax'))
+
+        model.compile(optimizer='adam',
+                  loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+        return model
