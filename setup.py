8dee6f7c1914d53ab75ec768b0494d3f70122c3d
Chu Qinghao <6337103+qinghao1@users.noreply.github.com>
2020-06-05 21:02:41 +0800

    Remove redundant requirement

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'pytesseract', 'pyautogui', 'psutil', 'pynput', 'opencv-python', 'pillow', 'pydirectinput', 'tensorflow', 'numpy', 'opencv-python'
+    'pytesseract', 'pyautogui', 'psutil', 'pynput', 'opencv-python', 'pillow', 'pydirectinput', 'tensorflow', 'numpy'
 ]
 
 # What packages are optional?
